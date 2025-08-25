0a4f701081469f1256b4a60e442b205722524324
Darkksideyoda <bahtiyar093@gmail.com>
2025-08-25 03:42:41 +0300

    cnf push

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   eslint:{
     ignoreDuringBuilds:true
   },
+  
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
