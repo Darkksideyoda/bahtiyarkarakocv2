5c2f71f2c9b5d913a1778174731ad4f155db54bc
Darkksideyoda <bahtiyar093@gmail.com>
2025-08-25 04:20:40 +0300

    cnf psh

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,6 @@ const nextConfig: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
   },
-  
   experimental: {
     optimizePackageImports: ['framer-motion', 'lucide-react'],
   },
